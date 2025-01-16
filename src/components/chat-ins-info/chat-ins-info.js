31825947992e72e5f98e5b5fec85b3538c0d0cf0
Amassi-Ismail <ismail02@my.yorku.ca>
2025-01-16 09:50:19 -0500

    User container info

--- src/components/chat-ins-info/chat-ins-info.js
+++ src/components/chat-ins-info/chat-ins-info.js
@@ -1,84 +1,20 @@
 import "./chat-ins-info.css"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faArrowDown, faArrowUp, faCancel, faDownload, faSignOut} from "@fortawesome/free-solid-svg-icons";
+import {faCancel, faSignOut} from "@fortawesome/free-solid-svg-icons";
 import {auth} from "../../config/firebase";
+import {chatStore} from "../../config/chatState";
+
 
 const ChatInsInfo = () => {
+    const { user } = chatStore();
+
     return (<div className="chat-ins-info">
         <div className='chat-ins-info'>
             <div className="user">
-                <img src='' alt="user-chat"/>
-                <h2>Test test</h2>
-                <p>Lorem ipsum dolor sit amet.</p>
+                <img src={user.avatar} alt="user-chat"/>
+                <h2>{user.username}</h2>
             </div>
             <div className="info">
-                <div className="option">
-                    <div className="title">
-                        <span>Chat Settings</span>
-                        {/*<img src="./arrowUp.png" alt=""/>*/}
-                        <FontAwesomeIcon className='icon-option' icon={faArrowUp}></FontAwesomeIcon>
-                    </div>
-                </div>
-                <div className="option">
-                    <div className="title">
-                        <span>Privacy & help</span>
-                        {/*<img src="./arrowUp.png" alt=""/>*/}
-                        <FontAwesomeIcon className='icon-option' icon={faArrowUp}></FontAwesomeIcon>
-                    </div>
-                </div>
-                <div className="option">
-                    <div className="title">
-                        <span>Shared photos</span>
-                        {/*<img src="./arrowDown.png" alt=""/>*/}
-                        <FontAwesomeIcon className='icon-option' icon={faArrowDown}></FontAwesomeIcon>
-                    </div>
-                    <div className="photos">
-                        <div className="photoItem">
-                            <div className="photoDetail">
-                                <img
-                                    src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
-                                    alt=""
-                                />
-                                <span>photo_2024_2.png</span>
-                            </div>
-                            {/*<img src="./download.png" alt="" className="icon"/>*/}
-                            <FontAwesomeIcon className='icon-option' icon={faDownload}></FontAwesomeIcon>
-                        </div>
-                        <div className="photoItem">
-                            <div className="photoDetail">
-                                <img
-                                    src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
-                                    alt=""
-                                />
-                                <span>photo_2024_2.png</span>
-                            </div>
-                            {/*<img src="./download.png" alt="" className="icon"/>*/}
-                            <FontAwesomeIcon className='icon-option' icon={faDownload}></FontAwesomeIcon>
-                        </div>
-                        <div className="photoItem">
-                            <div className="photoDetail">
-                                <img
-                                    src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
-                                    alt=""
-                                />
-                                <span>photo_2024_2.png</span>
-                            </div>
-                            {/*<img src="./download.png" alt="" className="icon"/>*/}
-                            <FontAwesomeIcon className='icon-option' icon={faDownload}></FontAwesomeIcon>
-                        </div>
-                        <div className="photoItem">
-                            <div className="photoDetail">
-                                <img
-                                    src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
-                                    alt=""
-                                />
-                                <span>photo_2024_2.png</span>
-                            </div>
-                            {/*<img src="./download.png" alt="" className="icon"/>*/}
-                            <FontAwesomeIcon className='icon-option' icon={faDownload}></FontAwesomeIcon>
-                        </div>
-                    </div>
-                </div>
                 <button /*onClick={handleBlock}*/>
                     {/*{isCurrentUserBlocked*/}
                     {/*    ? "You are Blocked!"*/}
