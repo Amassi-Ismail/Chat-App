e8a249ff670506942ca792a1226fe6a35014dfb4
Amassi-Ismail <ismail02@my.yorku.ca>
2025-03-27 14:05:42 -0400

    Update Firebase configuration to use environment variables for improved security

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,9 +2,16 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { initializeApp } from "firebase/app";
-import {toast} from "react-toastify";
+import { toast } from "react-toastify";
 
-//TODO: HERE NEEDS TO GO YOUR FIREBASE CONFIGURATION NEEDED FOR STORAGE, DATABASE AND AUTHENTICATION
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,         // Changed from APP_ID
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
 
 const app = initializeApp(firebaseConfig);
 
